9604574be28fcba05bb3d234e2989202f9e84713
VPspy2000 <68804@stpeters.sa.edu.au>
2024-08-05 18:36:45 +0930

    Add files via upload

---
+++ eibmoz.js
@@ -0,0 +1,22 @@
+AFRAME.registerComponent('zombiefest', {
+    init: function(){
+        var posX= -50;
+        var posY= -3;
+        var posZ= -150;
+        for ( var i = 1;i <= 3;i++){
+            var id ='zomb${i}';
+            var position = {x:posX, y: posY, z: posZ};
+            posX += 50;
+            this.zombieSpawn(id, position);
+        }
+    }, 
+    zombieSpawn: function (id, position) {
+var environEl= domcument.querySelector('a-scene');
+var zombEl = document.createElement('a-entity');
+zombEl.setAttribute('id', id);
+zombEl.setAttribute('position',position);
+zombEl.setAttribute('scale','10 10 10');
+zombEl.setAttribute('gltf-model','#zmb' );
+}
+})
+
